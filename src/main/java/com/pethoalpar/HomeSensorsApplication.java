d8824110dd378ae07cb034db476e37337e60e11c
pethoalpar <pethoalpar@gmail.com>
2019-06-13 07:45:47 +0300

    first commit

---
+++ src/main/java/com/pethoalpar/HomeSensorsApplication.java
@@ -0,0 +1,13 @@
+package com.pethoalpar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSensorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeSensorsApplication.class, args);
+	}
+
+}
